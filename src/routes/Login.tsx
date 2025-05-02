e69f01ecea343f06830a4b85580fb0348f4c0cb9
Christopher Aitken <chris@heythisischris.com>
2025-05-02 11:49:37 -0500

    Scaffolded React Vite SPA w/ basic packages, routes, & branding

---
+++ src/routes/Login.tsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import * as Auth from 'aws-amplify/auth';;
+import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import { useGlobalStore } from '#src/utils';
+import { useQueryClient } from '@tanstack/react-query';
+import { usePostHog } from 'posthog-js/react';
+import { IoPersonCircle } from 'react-icons/io5';
+
+export const Login = () => {
+    const queryClient = useQueryClient();
+    const loading = useGlobalStore(state => state.loading);
+    const setLoading = useGlobalStore(state => state.setLoading);
+    const setAuthenticated = useGlobalStore(state => state.setAuthenticated);
+    const [error, setError] = useState('');
+    const [checked, setChecked] = useState(true);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const posthog = usePostHog();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const email = searchParams.get('email');
+    const code = searchParams.get('code');
+    const type = searchParams.get('type');
+
+    useEffect(() => {
+        if (code && type) {
+            setError(`You must login before you can verify your new ${type?.replace('_', ' ')}`)
+        }
+    }, [code, type]);
+
+    const login = async (e) => {
+        e.preventDefault();
+        setError('');
+        setLoading('Logging in...');
+        const { email, password } = Object.fromEntries(new FormData(e.currentTarget));
+        try {
+            await Auth.signIn({
+                username: email.toString(),
+                password: password.toString(),
+            });
+            const attributes = (await Auth.fetchAuthSession()).tokens?.idToken?.payload;
+            setAuthenticated(attributes?.sub);
+            setLoading(false);
+            posthog?.identify(attributes?.sub, { email: attributes?.email });
+
+            if (code) {
+                navigate(`/verify/${location.search}`);
+            }
+            else {
+                navigate('/dashboard');
+            }
+
+            const hasuraClaims = JSON.parse(attributes?.['https://hasura.io/jwt/claims']);
+            if (hasuraClaims?.['x-hasura-default-role'] === 'cancelled') {
+                navigate('/subscriptions');
+            }
+            setTimeout(() => queryClient.refetchQueries({ queryKey: ['user'] }), 100);
+        }
+        catch (err) {
+            console.log(err);
+            setLoading(false);
+            setError(confirm?.error?.message ?? `There was an issue logging in. Please try again.`);
+        }
+    }
+
+    return (<form onSubmit={login} className='flex flex-col gap-4 max-w-screen-sm mx-auto pt-24'>
+        <h1 className='mx-auto'>Welcome back!</h1>
+        <div className='flex flex-col'>
+            <label htmlFor='email'>Email address</label>
+            <input id='email' name="email" required type="email" placeholder='johndoe@example.com'/>
+        </div>
+        <div className='flex flex-col'>
+            <label htmlFor='password'>Password</label>
+            <input id='password' name="password" required type="password" placeholder='Password'/>
+        </div>
+        <button disabled={loading} className=''>Log In</button>
+        {error && <div className=''>{error}</div>}
+    </form>)
+}
